9d107bd969db69085210d1650fa73ccb0a744886
xbl <x_bl@sina.com>
2025-08-31 14:15:25 +0800

    build: 去掉参数必须加括号的规则

--- my-app/eslint.config.js
+++ my-app/eslint.config.js
@@ -32,6 +32,7 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      '@stylistic/arrow-parens': 'off',
     },
   },
   {
