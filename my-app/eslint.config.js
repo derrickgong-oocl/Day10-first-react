1c8f1c12b8802b18ea31ee2a4ae6ae31e5d4a0bc
xbl <x_bl@sina.com>
2025-08-31 12:24:59 +0800

    feat: 增加严格的eslint 规则

--- my-app/eslint.config.js
+++ my-app/eslint.config.js
@@ -1,13 +1,21 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import airbnbConfig from 'airbnb-eslint9';
+import stylistic from '@stylistic/eslint-plugin';
+import importPlugin from 'eslint-plugin-import';
 
 export default defineConfig([
   globalIgnores(['dist']),
+  ...airbnbConfig,
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      '@stylistic': stylistic,
+      import: importPlugin,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
@@ -26,4 +34,11 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
-])
+  {
+    files: ['eslint.config.js', 'vite.config.js'],
+    rules: {
+      'import/no-extraneous-dependencies': 'off',
+      'import/no-unresolved': 'off',
+    },
+  },
+]);
